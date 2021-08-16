338b421a8291d886eeb8de01c0daf9965d0390ec
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-08-16 23:21:04 +0300

    change functions into classes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import React from "react";
 import "./App.css";
 
-function Header() {
-  return <h2>Welcome to IronHack Cinema</h2>;
+class Header extends React.Component {
+  render() {
+    return <h2>Welcome to IronHack Cinema</h2>;
+  }
 }
 
-function Main() {
-  return (
-    <main>
-      <Movie title="The Motorcycle Diaries" />
-      <Movie title="An Education" />
-      <Movie title="Green Mile" />
-      <Movie title="Inception" />
-      <Movie title="Butterfly Effect" />
-      <Movie title="The Matrix" />
-    </main>
-  );
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <Movie title="The Motorcycle Diaries" />
+        <Movie title="An Education" />
+        <Movie title="Green Mile" />
+        <Movie title="Inception" />
+        <Movie title="Butterfly Effect" />
+        <Movie title="The Matrix" />
+      </main>
+    );
+  }
 }
 
-function Movie(props) {
-  return (
-    <div className="movie">
-      <h5>{props.title}</h5>
-    </div>
-  );
+class Movie extends React.Component {
+  render() {
+    return (
+      <div className="movie">
+        <h5>{this.props.title}</h5>
+      </div>
+    );
+  }
 }
 
-function Footer() {
-  return <footer>&copy; 2021 Created with love by team at Ironhack</footer>;
+class Footer extends React.Component {
+  render() {
+    return <footer>&copy; 2021 Created with love by team at Ironhack</footer>;
+  }
 }
 
 function App() {
