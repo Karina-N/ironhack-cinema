9aa28aca1016163de0031a7c51ffdd8e85696254
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-08-16 18:59:33 +0300

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+function Header() {
+  return <h2>Welcome to IronHack Cinema</h2>;
+}
+
+function Main() {
+  return (
+    <main>
+      <Movie title="The Motorcycle Diaries" />
+      <Movie title="An Education" />
+      <Movie title="Green Mile" />
+      <Movie title="Inception" />
+      <Movie title="Butterfly Effect" />
+      <Movie title="The Matrix" />
+    </main>
+  );
+}
+
+function Movie(props) {
+  return (
+    <div className="movie">
+      <h5>{props.title}</h5>
+    </div>
+  );
+}
+
+function Footer() {
+  return <footer>&copy; 2021 Created with love by team at Ironhack</footer>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
