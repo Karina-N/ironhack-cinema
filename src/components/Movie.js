7751865fb0f3f14906ed7920eac52b4b0d4d3830
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-08-18 12:09:30 +0300

    render movie list with map & spread op

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -17,11 +17,31 @@ class Movie extends React.Component {
   };
 
   render() {
+    // OPTION 1
+    // let classList = this.state.value >= 5 ? " movie-popular" : "";
+
+    // OPTION 2
+    // if (this.state.value >= 5) {
+    //   classList += " movie-popular";
+    // }
+
+    const styleObj = {
+      color: "grey",
+      backgroundColor: this.state.value > 4 ? "yellowgreen" : "white",
+    };
+
     return (
-      <div className="movie">
-        <h3>{this.props.title}</h3>
-        <p>Number of likes: {this.state.value}</p>
-        <button onClick={this.increaseLikes}>Like</button>
+      // <div className={"movie " + classList}>
+      <div className="movie" style={styleObj}>
+        <img src={this.props.imgURL} alt={this.props.title + " poster"} />
+        <div>
+          <h3>{this.props.title}</h3>
+          <p>{this.props.year}</p>
+          <p>Rating: {this.props.rating}</p>
+          <p>Genres: {this.props.genres}</p>
+          <p>Number of likes: {this.state.value}</p>
+          <button onClick={this.increaseLikes}>Like</button>
+        </div>
       </div>
     );
   }
